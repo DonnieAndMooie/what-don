93f56c8805c6765c9ac01378c53df2f96e398ab0
Owen Eaves <owenjeaves@gmail.com>
2023-09-07 14:26:27 +0100

    Add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import logo from "../images/logo.png"
+
+export default function Header() {
+  return (
+    <header>
+        <img src={logo} alt="MK Dons Badge" />
+        <h1>What Don?</h1>
+    </header>
+  )
+}
